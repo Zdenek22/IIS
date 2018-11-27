04dc8e48076dc9ee780eadf4153cfeeab7e4f40e
Zdenek22 <32484343+Zdenek22@users.noreply.github.com>
2018-11-27 01:32:47 +0100

    diakritika funguje

--- invalidLogin.php
+++ invalidLogin.php
@@ -1,28 +1,29 @@
 <?php
 
 function invalidLoginPage(){
-  ?>
+  header("Content-Type: text/html; charset=UTF-8");
+?>
 
 <!DOCTYPE html>
 <html lang="cz">
 
 <head>
-    <link rel="stylesheet" type="text/css" href="style.css">
-    <link rel="icon" type="image/x-icon" href="snake.png">
-    <title>Lékárna - Chyba přihlášení</title>
+  <link rel="stylesheet" type="text/css" href="style.css">
+  <link rel="icon" type="image/x-icon" href="snake.png">
+  <title>Lékárna - Chyba přihlášení</title>
 </head>
 
 <body>
 	<h1>Lékárna</h1>
 	<div class="imgcontainer">
     	<img src="snake.png" class="logo">
-  	</div>
-  	<div class="error">
+  </div>
+  <div class="error">
 		Nesprávné jméno nebo heslo!<br>
 		<form class="errorForm" action="index.html">
 			<button class="errorButton">Zpět na přihlášení</button>			
 		</form>
-  	</div>
+  </div>
 </body>
 </html>
 
