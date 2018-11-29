0ea66f88fb4f404e8449bfdc378eefb7d4fdbb37
Zdenek22 <32484343+Zdenek22@users.noreply.github.com>
2018-11-30 00:25:13 +0100

    Add files via upload

--- reservpage.php
+++ reservpage.php
@@ -33,7 +33,7 @@ function makeReservPage(){
 	$server = new Database_access();
 	$user = $server->getInformation($_SESSION['user']);
 	?>
-		<h1>Lékárna - <? echo $user['pobocka']; ?></h1>
+		<h1>Lékárna - pobočka <? echo $user['pobocka']; ?></h1>
 		<div style="margin-bottom: 2cm"></div>
 	<?php
 }
