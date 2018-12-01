e0996a87dbb7e77312e126a0f9c7576cee0f3ab3
Zdenek22 <32484343+Zdenek22@users.noreply.github.com>
2018-12-01 23:26:38 +0100

    Add files via upload

--- makereservpage.php
+++ makereservpage.php
@@ -129,7 +129,107 @@ function endMakeReservPage(){
 	</html>
 	<?php
 }
+
+
+function fillAgain($fill, $errorMsg, $count, $medicine){
+	$predpis = -1;
+	if($medicine['predpis'] == 1)
+		$predpis = "Ano";
+	else
+		$predpis = "Ne";
+
+	$popis = "";
+	if(isset($medicine['popis']))
+		$popis = $medicine['popis'];		
+	?>	
+	<div class="secondCol">
+		<div class="medicineInfo">
+			<img src="bottle.png" style="width: 20%; height: 20%; float: left;">
+			<div class="text">
+				<span class="medDesc">Lek: </span>
+				<span class="medVal"><? echo $medicine['jmeno']; ?></span><br>
+				<span class="medDesc">Cena: </span>
+				<span class="medVal"><? echo $medicine['cena']; ?></span><br>
+				<span class="medDesc">Na předpis: </span>
+				<span class="medVal"><? echo $predpis ?></span><br>
+				<span class="medDesc">Skladem: </span>
+				<span class="medVal"><? echo $medicine['pocet']; ?></span><br>
+				<span class="medDesc">Popis: </span>
+				<span class="medVal"><? echo $popis; ?></span>
+			</div>
+		</div>	
+		<form action="reservationProceed.php" method="post">
+			<div class="userForm" style="float: left">
+				<caption>Vyplňte formulář (všechny položky jsou povinné):</caption><br>
+				<input type="hidden" name="lek" value=<?echo '"';echo $medicine['jmeno'];echo '"';?>>
+
+				<table style="float: left;">
+					<tr>
+						<td><span class="formDesc">Jméno</span></td>
+						<td><input class="userInput" type="text" name="jmeno" value=<?echo '"';echo $fill['jmeno'];echo '"';?> required="required"></td>
+					</tr>
+					<tr>
+						<td><span class="formDesc">Příjmení</span></td>
+						<td><input class="userInput" type="text" name="prijmeni" value=<?echo '"';echo $fill['prijmeni'];echo '"';?> required="required"></td>
+					</tr>
+					<tr>
+						<td><span class="formDesc">Rodné číslo</span></td>
+						<td><input class="userInput" type="text" name="RC" value=<?echo '"';echo $fill['RC'];echo '"';?> required="required"></td>
+					</tr>
+					<tr>
+						<td><span class="formDesc">Pojišťovna</span></td>
+						<td><input class="userInput" type="text" name="pojistovna" value=<?echo '"';echo $fill['pojistovna'];echo '"';?> required="required"></td>
+					</tr>
+				</table>	
+			</div>
+			<table class="amount">
+				<tr>
+					<td>Množství léku (ks):</td>
+					<td><input type="number" name="amount" value="1" min="1"></td>
+				</tr>	
+				<tr>
+					<td>Vyberte pobočku:</td>
+					<td>
+						<input list="pobocka" name="pobocka" required="required">
+						<datalist id="pobocka">
+  							<option value="U Raka">
+  							<option value="Španělská">
+  							<option value="U Černé bobule">
+  							<option value="Babiččina lékárna">
+						</datalist>
+  					</td>
+				</tr>
+			</table>
+			<div class="reservMedButtons">
+				<input class="resButt" type="submit" name="rezervovat" value="Rezervovat">
+			</div>
+		</form>
+		<form action="main.php">
+			<table class="helpTable">
+				<tr>
+					<td style="width: 80%;"></td>
+					<td><input class="cancButt" type="submit" name="zrusit" value="Zrušit"><td>
+				</tr>	
+			</table>			
+		</form>
+		<br>
+		<?php
+		foreach ($errorMsg as $key => $value) {
+		?>
+		<div style="float: left;margin: 4px; width: 100%;">
+			<?echo $value;?>
+		<br>
+		</div>
+		<?php
+	}
+	?>
+	</div>		
+	<?php
+				
+}
+
 ?>
+
 
 
 
