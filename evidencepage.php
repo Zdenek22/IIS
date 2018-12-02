8b38bb251b69182de17c36104029f0906601fd40
Zdenek22 <32484343+Zdenek22@users.noreply.github.com>
2018-12-02 22:39:34 +0100

    Add files via upload

--- evidencepage.php
+++ evidencepage.php
@@ -42,17 +42,17 @@ function makeEvidencePage(){
 function startEvidenceTable(){
 	?>
 	<div class="secondCol">
-	<table class="evidenceTable">
+	<table style="width: 100%;border: 1px solid #ccc; border-collapse: collapse;">
 				<!--cas, kdo, co, komu, pojistovna, kolik	-->	
 			<tr>
-				<th>Čas vydání</th>
-				<th>Vydal</th>
-				<th>Rodné číslo zákazníka</th>
-				<th>Jméno zákazníka</th>
-				<th>Příjmení zákazníka</th>
-				<th>Pojišťovna</th>
-				<th>Lék</th>
-				<th>Množství (ks)</th>
+				<th style="border-collapse: collapse;text-align: center;border: 1px solid #ccc;">Čas vydání</th>
+				<th style="border-collapse: collapse;text-align: center;border: 1px solid #ccc;">Vydal</th>
+				<th style="border-collapse: collapse;text-align: center;border: 1px solid #ccc;">Rodné číslo zákazníka</th>
+				<th style="border-collapse: collapse;text-align: center;border: 1px solid #ccc;">Jméno zákazníka</th>
+				<th style="border-collapse: collapse;text-align: center;border: 1px solid #ccc;">Příjmení zákazníka</th>
+				<th style="border-collapse: collapse;text-align: center;border: 1px solid #ccc;">Pojišťovna</th>
+				<th style="border-collapse: collapse;text-align: center;border: 1px solid #ccc;">Lék</th>
+				<th style="border-collapse: collapse;text-align: center;border: 1px solid #ccc;">Množství (ks)</th>
 			</tr>
 	<?		
 }
@@ -61,14 +61,14 @@ function startEvidenceTable(){
 function fillEvidenceTable($transakce){
 	?>	
 	<tr>
-		<td><?echo $transakce['cas'];?></td>
-		<td><?echo $transakce['login'];?></td>
-		<td><?echo $transakce['RC'];?></td>
-		<td><?echo $transakce['jmeno'];?></td>
-		<td><?echo $transakce['prijmeni'];?></td>
-		<td><?echo $transakce['pojistovna'];?></td>
-		<td><?echo $transakce['lek'];?></td>
-		<td><?echo $transakce['kolik'];?></td>
+		<td style="border-collapse: collapse;text-align: center;border: 1px solid #ccc;"><?echo $transakce['cas'];?></td>
+		<td style="border-collapse: collapse;text-align: center;border: 1px solid #ccc;"><?echo $transakce['login'];?></td>
+		<td style="border-collapse: collapse;text-align: center;border: 1px solid #ccc;"><?echo $transakce['RC'];?></td>
+		<td style="border-collapse: collapse;text-align: center;border: 1px solid #ccc;"><?echo $transakce['jmeno'];?></td>
+		<td style="border-collapse: collapse;text-align: center;border: 1px solid #ccc;"><?echo $transakce['prijmeni'];?></td>
+		<td style="border-collapse: collapse;text-align: center;border: 1px solid #ccc;"><?echo $transakce['pojistovna'];?></td>
+		<td style="border-collapse: collapse;text-align: center;border: 1px solid #ccc;"><?echo $transakce['lek'];?></td>
+		<td style="border-collapse: collapse;text-align: center;border: 1px solid #ccc;"><?echo $transakce['kolik'];?></td>
 	</tr>	
 	<?php
 }
