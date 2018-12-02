4647231a38f32f20320500e3ca07b9ab31b0fff6
AdamGregor <spectroa@gmail.com>
2018-12-02 16:40:24 +0100

    Add files via upload

---
+++ sell.php
@@ -0,0 +1,35 @@
+<?
+session_save_path("./tmp");
+session_start();
+
+require_once "DBOperations.php";
+require_once "supFunct.php";
+require_once "sellpage.php";
+require_once "mainpage.php";
+require_once "accountinfo.php";
+
+$server = new Database_access();
+
+$idLeku = $server->getMedsID($_POST['lek']);
+$cenaLeku = $server->getMedsValue($idLeku);
+
+$params['jmeno'] = $_POST['lek'];
+$params['pocet'] = $_POST['amount'];
+$params['cenakus'] = $cenaLeku;
+$params['sleva'] = $_POST['vydat'];
+if(isset($_POST['RC']))
+	$params['RC'] = $_POST['RC'];
+if(isset($_POST['pojistovna']))
+	$params['pojistovna'] = $_POST['pojistovna'];
+$params['predpis'] = $_POST['predpis'];
+
+
+
+makeSellPage();
+mainPageButtons();
+overview($params);
+userAccountInfo();
+endSellPage();
+
+
+?>
