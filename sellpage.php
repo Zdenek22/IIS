13f909921a90fa3fddaad253ce0c984e5958c1ac
Zdenek22 <32484343+Zdenek22@users.noreply.github.com>
2018-12-02 16:32:05 +0100

    Add files via upload

---
+++ sellpage.php
@@ -0,0 +1,182 @@
+<?php
+
+// funkce pro vytvoreni stranky pro prodej leku
+
+
+// nektere funkce jsou v mainpage.php, takto se zkompletuje stranka
+// usage:	makeSellPage()
+//			mainPageButtons()
+//			overview($info)/overviewReserv($info) - podle toho, jestli se tam jde z rezervace nebo pres lek
+//			userAccountInfo()
+//			endSellPage()
+
+
+require_once "DBOperations.php";
+
+// zacatek stranky s rezervacemi
+function makeSellPage(){
+	header("Content-Type: text/html; charset=UTF-8");
+	?>
+	<!DOCTYPE html>
+	<html lang="cz">
+
+	<head>
+   	 <link rel="stylesheet" type="text/css" href="mainstyle.css">
+   	 <link rel="icon" type="image/x-icon" href="snake.png">
+   	 <title>Lékárna - prodej léku</title>
+	</head>
+	<body>
+	<?php
+	$server = new Database_access();
+	$user = $server->getInformation($_SESSION['user']);
+	?>
+		<h1>Lékárna - pobočka <? echo $user['pobocka']; ?></h1>
+		<div style="margin-bottom: 2cm"></div>
+	<?php
+}
+
+// zobrazi prehled k prodeji leku
+function overview($info){
+	$jmeno = $info['jmeno'];
+	$pocet = $info['pocet'];
+	$kus = $info['cenakus'];
+	$sleva="";$RC="";$pojistovna="";$predpis=0;
+	if($info['predpis'] == '1'){
+		$sleva = $info['sleva'];
+		$RC = $info['RC'];
+		$pojistovana = $info['pojistovna'];
+		$predpis = 1;
+	}	
+
+	$celkem = 0;
+	if($predpis == 1){
+		$celkem = ($pocet * $kus) - ($pocet * $sleva);
+	}	
+	else{
+		$celkem = $pocet * $kus;
+	}
+	?>
+	<div class="secondCol">
+		<table class="overviewTable">
+			<tr>
+				<td class="otd">Lék:</td>
+				<td class="mtd"><?echo $jmeno;?></td>
+			</tr>
+			<tr>
+				<td class="otd">Množství:</td>
+				<td class="mtd"><?echo $pocet;?> Ks</td>
+			</tr>
+			<tr>
+				<td class="otd">Cena za kus:</td>
+				<td class="mtd"><?echo $kus;?> Kč</td>
+			</tr>
+			<?php
+			if($predpis == 1){
+				?><tr>
+					<td class="otd">Rodné číslo:</td>
+					<td class="mtd"><?echo $RC;?></td>
+				</tr>
+				<tr>
+					<td class="otd">Pojišťovna:</td>
+					<td class="mtd"><?echo $pojistovna;?></td>
+				</tr>
+				<tr>
+					<td class="otd">Sleva od pojišťovny:</td>
+					<td class="mtd"><?echo $sleva;?> Kč</td>
+				</tr>
+				<?php
+			}
+			?>
+				<tr>
+					<td class="otd">Cena celkem:</td>
+					<td class="mtd"><?echo $celkem;?> Kč</td>
+				</tr>	
+
+			<tr>
+				<td class="otd">
+					<form action="main.php">
+						<input type="submit" name="cancel" value="Storno" style="background-color: red;color: white;">
+					</form>
+				</td>
+				<td>
+					<form action="operations.php" method="post">
+						<input type="hidden" name="lek" value=<?echo '"';echo $info['jmeno'];echo '"';?>>
+						<input type="hidden" name="RC" value=<?echo '"';echo $info['RC'];echo '"';?>>
+						<input type="hidden" name="pocet" value=<?echo '"';echo $info['pocet'];echo '"';?>>
+						<input type="hidden" name="pojistovna" value=value=<?echo '"';echo $info['pocet'];echo '"';?>>
+						<input type="submit" name="finish" value="Dokončit" style="background-color: #4CAF50;color: white;">
+					</form>
+				</td>
+			</tr>
+		</table>	
+	</div>
+	<?php
+}
+
+
+function overviewReserv($count, $info, $zakaznik, $pobocka){
+	// 'lek' - jmeno leku, 'rezervace' -> prispevek na lek, 'neslevnenaCena'->cena za kus, 'pocet' -> mnozstvi leku, 
+	// 'celkemBezSlevy' -> pobocka celkem, 'celkemSeSlevou'
+	?>
+	<div class="secondCol">
+		<table class="overviewTable">
+		<?php
+		for ($i=0; $i < $count; $i++) { 
+			?>
+			<tr>
+				<td class="otd">Lék:</td>
+				<td class="mtd"><?echo $info[$i]['lek'];?></td>
+			</tr>
+			<tr>
+				<td class="otd">Množství:</td>
+				<td class="mtd"><? echo $info[$i]['pocet']; ?> Ks</td>
+			</tr>
+			<tr>
+				<td class="otd">Cena za kus:</td>
+				<td class="mtd"><?echo $info[$i]['neslevnenaCena'];?> Kč</td>
+			</tr>
+			<tr>
+				<td class="otd">Sleva od pojišťovny:</td>
+				<td class="mtd"><? echo $info[$i]['rezervace']; ?></td>
+			</tr>
+			<tr>
+				<td class="otd"></td>
+				<td class="mtd"></td>
+			</tr>
+			<?php
+		}
+		?>
+		<tr>
+			<td class="otd">Cena celkem bez slevy:</td>
+			<td class="mtd"><?echo $pobocka['celkemBezSlevy'];?> Kč</td>
+		</tr>
+		<tr>
+			<td class="otd">Cena celkem se slevou:</td>
+			<td class="mtd"><?echo $zakaznik['celkemSeSlevou'];?> Kč</td>
+		</tr>
+		<tr>
+			<td class="otd">
+				<input type="submit" name="cancel" value="Storno" style="background-color: red;color: white;">
+			</td>
+			<td>
+				<input type="hidden" name="pocet" value="">
+				<input type="submit" name="finish" value="Dokončit" style="background-color: #4CAF50;color: white;">
+			</td>
+		</tr>
+		</table>
+	</div>	
+	<?php
+}
+
+
+// ukonceni stranky
+function endSellPage(){
+	?>
+			</div> <!-- konec thirdCol -->
+		</body>
+	</html>
+	<?php
+}
+?>
+
+
