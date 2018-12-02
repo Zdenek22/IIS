04b7c8d1b8d3573412b47422b77949da539c3af5
Zdenek22 <32484343+Zdenek22@users.noreply.github.com>
2018-12-02 20:12:39 +0100

    Add files via upload

--- mainpage.php
+++ mainpage.php
@@ -208,6 +208,13 @@ function userInfo($id){
 		Telefon: <? echo $phone; ?><br>
 		Status: <? echo $status; ?><br>
 		Pobočka: <? echo $user['pobocka']; ?><br>
+		<?php
+		if($user['postaveni'] == 1){
+			?>
+			Peněz na pobočce: <?echo "xxx";?> Kč<br>
+			<?php
+		}
+		?>
 	</div>
 	<br>
 	<form action="account.php" method="get">
