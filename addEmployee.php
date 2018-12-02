b9c77eefbadc8e8a2d684e0cd04d4d8174d51333
Zdenek22 <32484343+Zdenek22@users.noreply.github.com>
2018-12-02 23:34:51 +0100

    Add files via upload

--- addEmployee.php
+++ addEmployee.php
@@ -10,7 +10,45 @@ require_once "accountinfo.php";
 
 makeAddEmployeePage();
 mainPageButtons();
-addEmployeeForm();
+
+
+if(isset($_POST['login'])){
+
+	$fill;
+	$errorMsg[0] = "";
+	$count = 0;
+
+	$fill['heslo'] = $_POST['heslo'];
+	$fill['login'] = $_POST['login'];
+	$fill['jmeno'] = $_POST['jmeno'];
+	$fill['prijmeni'] = $_POST['prijmeni'];
+	$fill['telefon'] = $_POST['telefon'];
+	$fill['email'] = $_POST['email'];
+	$fill['pobocka'] = $_POST['pobocka'];
+	$fill['postaveni'] = $_POST['postaveni'];
+
+	if($_POST['login'] == '0'){
+		$fill = "";
+		$errorMsg[$count] = "Zadaný login již existuje! Zadejte jiný.";
+		$count = $count + 1;
+	}
+	if($_POST['jmeno'] == '0'){
+		$fill['jmeno'] = "";
+		$errorMsg[$count] = 'Špatný formát jména!';
+		$count = $count + 1;
+	}
+	if($_POST['prijmeni'] == '0'){
+		$fill['prijmeni'] = "";
+		$errorMsg[$count] = "Špatný formát příjmení! Zadejte znovu.";
+		$count = $count + 1;
+	}
+
+	fillEmployeeAgain($fill, $errorMsg, $count);
+}
+
+else{
+	addEmployeeForm();
+}
 
 userAccountInfo();
 endAddEmployeePage();
--- addemployeepage.php
+++ addemployeepage.php
@@ -101,6 +101,78 @@ function addEmployeeForm(){
 }
 
 
+function fillEmployeeAgain($employee, $errormsg, $count){
+	?>
+	<div class="secondCol">
+		<form action="" method="post">
+			<table style="width: 100%;border: 1px solid #ccc;">
+				<tr>
+					<td style="width: 20%;">*Login zaměstnance:</td>
+					<td style="text-align: left;"><input type="text" name="login" required="required" placeholder="xlogin" value=<?echo '"';echo $employee['login'];echo '"';?>></td>
+				</tr>
+				<tr>
+					<td style="width: 20%;">*Heslo zaměstnance:</td>
+					<td style="text-align: left;"><input type="text" name="heslo" required="required" placeholder="xlogin" value=<?echo '"';echo $employee['heslo'];echo '"';?>></td>
+				</tr>
+				<tr>
+					<td style="width: 20%;">*Jméno zaměstnance:</td>
+					<td style="text-align: left;"><input type="text" name="jmeno" placeholder="Jméno" required="required" value=<?echo '"';echo $employee['jmeno'];echo '"';?>></td>
+				</tr>
+				<tr>
+					<td style="width: 20%;">*Příjmení zaměstnance:</td>
+					<td style="text-align: left;"><input type="text" name="prijmeni" placeholder="Příjmení" required="required" value=<?echo '"';echo $employee['prijmeni'];echo '"';?>></td>
+				</tr>
+				<tr>
+					<td style="width: 20%;"> Telefon zaměstnance:</td>
+					<td style="text-align: left;" style="text-align: left;"><input type="tel" name="telefon" pattern="[0-9]{3}[0-9]{3}[0-9]{3}" placeholder="909090909" value=<?echo '"';echo $employee['telefon'];echo '"';?>></td>
+				</tr>
+				<tr>
+					<td style="width: 20%;"> Email zaměstnance:</td>
+					<td style="text-align: left;"><input type="email" name="email" placeholder="xlogin@gmail.com" value=<?echo '"';echo $employee['email'];echo '"';?>></td>
+				</tr>
+				<tr>
+					<td style="width: 20%;"> Postavení zaměstnance:</td>
+					<td style="text-align: left;"><select list="postaveni" name="postaveni" required="required" style="width: 20%;">
+						<datalist id="postaveni">
+							<option value="0">Zaměstnanec</option>
+							<option value="1">Správce</option>
+						</datalist>
+					</td>	
+				</tr>
+				<tr>
+					<td style="width: 20%;">*Pobočka, na které bude pracovat:</td>
+					<td style="text-align: left;"><select list="pobocka" name="pobocka" required="required" style="width: 20%;">
+						<datalist id="list">
+								<?php
+
+								$server = new Database_access();
+								$pobo = $server->getAllPobocka();
+								foreach ($pobo as $key => $value) {
+						 			?>
+						 			<option><?echo $value['jmeno'];?></option>
+						 			<?php
+						 		} 
+								?>	
+							</datalist>
+							</td>
+				</tr>
+				<tr>
+					<td style="width: 20%;"></td>
+					<td style="text-align: center;"><input type="submit" name="addEmp" value="Přidat" style="color: white;background-color: #4CAF50;margin-right: 20%;"></td>
+				</tr>
+				<tr>
+					<td style="width: 20%">* - pole je povinné</td>
+				</tr>
+			</table>
+		</form>	
+	</div>
+	<?php
+}
+
+
+
+
+
 // ukonceni stranky
 function endAddEmployeePage(){
 	?>
