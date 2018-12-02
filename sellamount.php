47d4a7d36693ece6941b0343b272a6fa15249470
Zdenek22 <32484343+Zdenek22@users.noreply.github.com>
2018-12-02 14:10:00 +0100

    Add files via upload

--- sellamount.php
+++ sellamount.php
@@ -11,10 +11,55 @@ require_once "accountinfo.php";
 makeSellAmountPage();
 mainPageButtons();
 
-$server = new Database_access();
-$medicament = $server->getMedicament($_GET['lek'], $_SESSION['pobocka']);
+if(isset($_POST['lek'])){
 
-amountToSell($medicament[0]);
+	$fill;
+	$errorMsg;
+	$count = 0;
+	$server = new Database_access();
+	$medicament = $server->getMedicament($_POST['lek'], $_SESSION['pobocka']);
+
+	$fill['jmeno'] = $_POST['jmeno'];
+	$fill['prijmeni'] = $_POST['prijmeni'];
+	$fill['RC'] = $_POST['RC'];
+	$fill['pojistovna'] = $_POST['pojistovna'];
+
+	if($_POST['jmeno'] == '0'){
+		$fill['jmeno'] = "";
+		$errorMsg[$count] = 'Špatný formát jména!';
+		$count = $count + 1;
+	}
+	if($_POST['prijmeni'] == '0'){
+		$fill['prijmeni'] = "";
+		$errorMsg[$count] = "Špatný formát příjmení! Zadejte znovu";
+		$count = $count + 1;
+	}
+	if($_POST['RC'] == '0'){
+		$fill['RC'] = "";
+		$errorMsg[$count] = "Špatný formát rodného čísla! Zadejte znovu";
+		$count = $count + 1;
+	}	
+	elseif ($_POST['RC'] == '1') {
+		$fill['RC'] = "";
+		$fill['jmeno'] = $_POST['jmeno'];
+		$prijmeni['prijmeni'] = $_POST['prijmeni'];
+		$errorMsg[$count] = "Zadané rodné číslo odpovídá jinému člověku!";
+		$count = $count + 1;
+	}
+	if($_POST['pojistovna'] == '0'){
+		$fill['pojistovna'] = "";
+		$errorMsg[$count] = "Zadaná pojišťovna neexistuje! Zadejte znovu.";
+		$count = $count + 1;
+	}
+
+	fillSellAgain($fill, $errorMsg, $count, $medicament[0]);
+}
+
+else{
+	$server = new Database_access();
+	$medicament = $server->getMedicament($_GET['lek'], $_SESSION['pobocka']);
+	amountToSell($medicament[0]);
+}
 userAccountInfo();
 endSellAmountPage();
 ?>
--- sellamountpage.php
+++ sellamountpage.php
@@ -68,8 +68,9 @@ function amountToSell($medicine){
 		if($medicine['predpis'] == 1){
 			?>
 
-			<form>
+			<form action="operations.php" method="post">
 				<div class="userForm" style="float: left">
+					<input type="hidden" name="lek" value=<?echo '"';echo $medicine['jmeno'];echo '"';?>>
 					<caption>Vyplňte formulář (všechny položky jsou povinné):</caption><br>
 					<table style="float: left;">
 					<tr>
@@ -140,6 +141,115 @@ function endSellAmountPage(){
 	</html>
 	<?php
 }
+
+
+function fillSellAgain($fill, $errorMsg, $count, $medicine){
+	$predpis = -1;
+	if($medicine['predpis'] == 1)
+		$predpis = "Ano";
+	else
+		$predpis = "Ne";
+
+	$popis = "";
+	if(isset($medicine['popis']))
+		$popis = $medicine['popis'];
+	?>
+	<div class="secondCol">
+		<div class="medicineInfo">
+			<img src="bottle.png" style="width: 20%; height: 20%; float: left;">
+			<div class="text">
+				<span class="medDesc">Lék: </span>
+				<span class="medVal"><? echo $medicine['jmeno']; ?></span><br>
+				<span class="medDesc">Cena: </span>
+				<span class="medVal"><? echo $medicine['cena']; ?> Kč</span><br>
+				<span class="medDesc">Na předpis: </span>
+				<span class="medVal"><? echo $predpis ?></span><br>
+				<span class="medDesc">Skladem: </span>
+				<span class="medVal"><? echo $medicine['pocet']; ?> Ks</span><br>
+				<span class="medDesc">Popis: </span>
+				<span class="medVal"><? echo $popis; ?></span>
+			</div>
+		</div>
+		<?php
+		if($medicine['predpis'] == 1){
+			?>
+
+			<form>
+				<div class="userForm" style="float: left">
+					<caption>Vyplňte formulář (všechny položky jsou povinné):</caption><br>
+					<table style="float: left;">
+					<tr>
+						<td><span class="formDesc">Jméno</span></td>
+						<td><input class="userInput" type="text" name="jmeno" value=<?echo '"';echo $fill['jmeno'];echo '"';?> required="required"></td>
+					</tr>
+					<tr>
+						<td><span class="formDesc">Příjmení</span></td>
+						<td><input class="userInput" type="text" name="prijmeni" value=<?echo '"';echo $fill['prijmeni'];echo '"';?> required="required"></td>
+					</tr>
+					<tr>
+						<td><span class="formDesc">Rodné číslo</span></td>
+						<td><input class="userInput" type="text" name="RC" value=<?echo '"';echo $fill['RC'];echo '"';?> required="required"></td>
+					</tr>
+					<tr>
+						<td><span class="formDesc">Pojišťovna</span></td>
+						<td><input class="userInput" type="text" name="pojistovna" value=<?echo '"';echo $fill['pojistovna'];echo '"';?> required="required"></td>
+					</tr>
+				</table>			
+				</div>
+				<table class="amount">
+					<tr>
+						<td>Množství léku (ks):</td>
+						<td><input type="number" name="amount" value="1" min="1"></td>
+					</tr>	
+				</table>
+				<div class="reservMedButtons">
+					<input class="resButt" type="submit" name="vydat" value="Vydat lék">
+				</div>
+			</form>	
+			<form action="main.php">
+			<table class="helpTable">
+				<tr>
+					<td style="width: 80%;"></td>
+					<td><input class="cancButt" type="submit" name="zrusit" value="Zrušit"><td>
+				</tr>	
+			</table>			
+		</form>
+		
+		<?php
+		}
+		else{
+			?>
+			<form>
+				<div class="sellAmount">
+					<span class="amountDesc">Množství léku (ks):</span>
+					<input class="numAmount" type="number" name="amount" value="1" min="1">
+					<div class="sellMedButtons">
+						<input class="sellAmountButt" type="submit" name="vydat" value="Vydat">
+					</div>
+				</div>
+			</form>	
+			<form action="main.php" class="cancForm">
+				<input class="cancSellAmountButt" type="submit" name="zrusit" value="Zrušit">			
+			</form>
+		<?php
+		}
+		?>
+		<br>
+
+		<?php
+		foreach ($errorMsg as $key => $value) {
+		?>
+		<div style="float: left;margin: 4px; width: 100%;">
+			<?echo $value;?>
+		<br>
+		</div>
+		<?php
+	}
+	?>
+		?>			
+	<?php			
+}
+
 ?>
 
 
